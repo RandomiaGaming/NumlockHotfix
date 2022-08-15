7062574add338a37df65c94392b34bbc83fa72d0
Randomia Gaming <randomiagaming@gmail.com>
2022-08-15 00:13:59 -0700

    Initial Commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("NumlockHotfix")]
+[assembly: AssemblyDescription("Enables numlock and disables capslock to make signing in easier. Most useful when set to run on system startup via use of a scheduled task.")]
+[assembly: AssemblyCompany("RandomiaGaming")]
+[assembly: AssemblyProduct("NumlockHotfix")]
+[assembly: AssemblyCopyright("Copyright RandomiaGaming 2022")]
+[assembly: AssemblyTrademark("Trademark RandomiaGaming 2022")]
+[assembly: ComVisible(false)]
+[assembly: Guid("046d13af-07e2-49a1-8b9d-dae37bd74111")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+namespace NumlockHotfix
+{
+    public static class NumlockHotfix
+    {
+        private const byte VK_NUMLOCK = 144;
+        private const byte VK_CAPITAL = 20;
+        private const uint KeyDown = 0;
+        private const uint KeyUp = 2;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        private static extern short GetKeyState(int keyCode);
+        public static void Main()
+        {
+            if ((((ushort)GetKeyState(VK_CAPITAL)) & 0xffff) != 0)
+            {
+                keybd_event(VK_CAPITAL, 0, KeyDown, 0);
+                keybd_event(VK_CAPITAL, 0, KeyUp, 0);
+            }
+            if ((((ushort)GetKeyState(VK_NUMLOCK)) & 0xffff) == 0)
+            {
+                keybd_event(VK_NUMLOCK, 0, KeyDown, 0);
+                keybd_event(VK_NUMLOCK, 0, KeyUp, 0);
+            }
+        }
+    }
+}
